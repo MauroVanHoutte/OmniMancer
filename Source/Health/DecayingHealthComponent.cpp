ba957a856261005d7da9344349bb638ebd1e0eb4
MauroVanHoutte <68465103+MauroVanHoutte@users.noreply.github.com>
2024-09-05 12:42:38 +0200

    Added component that loses health over time

---
+++ Source/Health/DecayingHealthComponent.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Health/DecayingHealthComponent.h"
+
+void UDecayingHealthComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (!bIsDepleted && bIsDecaying)
+	{
+		CurrentHealth -= DecayRate * DeltaTime;
+
+		if (CurrentHealth < 0)
+		{
+			CurrentHealth = FMath::Clamp(CurrentHealth, 0, MaxHealth);
+			bIsDepleted = true;
+			bIsDecaying = false;
+		}
+		
+		if (BoundHealthbar)
+			BoundHealthbar->SetHealthPercentage(CurrentHealth / MaxHealth);
+	}
+}
+
+void UDecayingHealthComponent::Heal(float HealAmount)
+{
+	Super::Heal(HealAmount);
+
+	bIsDecaying = false;
+	FTimerDelegate Delegate;
+	Delegate.BindUObject(this, &UDecayingHealthComponent::StartDecaying);
+	GetWorld()->GetTimerManager().SetTimer(DecayDelayTimer, Delegate, DecayDelay, false);
+}
+
+void UDecayingHealthComponent::StartDecaying()
+{
+	bIsDecaying = true;
+}
---
+++ Source/Health/DecayingHealthComponent.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Health/BaseHealthComponent.h"
+#include "DecayingHealthComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECT_API UDecayingHealthComponent : public UBaseHealthComponent
+{
+	GENERATED_BODY()
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void Heal(float HealAmount) override;
+
+protected:
+	UFUNCTION()
+	void StartDecaying();
+
+	UPROPERTY(EditDefaultsOnly)
+	float DecayRate = 0.3f;
+	UPROPERTY(EditDefaultsOnly)
+	float DecayDelay = 3.f;
+	bool bIsDecaying = true;
+	FTimerHandle DecayDelayTimer;
+};
