d87a1c5ee7c9741fef5a41fe20dc212fb92e80a7
MauroVanHoutte <68465103+MauroVanHoutte@users.noreply.github.com>
2025-04-23 10:24:07 +0000

    Added new move requirements
    Added health requirement, limited uses requirement, range requirement, cooldown requirement, sphere trace requirement

--- Source/Enemies/Moves/MoveRequirements.cpp
+++ Source/Enemies/Moves/MoveRequirements.cpp
@@ -2,4 +2,63 @@
 
 
 #include "Enemies/Moves/MoveRequirements.h"
+#include "Health/HealthManager.h"
 
+bool UHealthMoveRequirement::AreRequirementsMet(AActor* Owner, AActor* Target)
+{
+	UHealthManager* ComponentToTest = bCheckOwnerHealth ? Owner->GetComponentByClass<UHealthManager>() : Target->GetComponentByClass<UHealthManager>();
+	float HealthTestValue = bUsePercentageThreshold ? ComponentToTest->GetTotalRemainingHealthPercentage() : ComponentToTest->GetTotalRemainingHealth();
+	return bRequireHealthBelowThreshold ? HealthTestValue < Threshold : HealthTestValue > Threshold;
+}
+
+bool ULimitedUsesRequirement::AreRequirementsMet(AActor* Owner, AActor* Target)
+{
+	return UseCounter < NumberOfUses;
+}
+
+void ULimitedUsesRequirement::OnAttackExecuted(AActor* Owner, AActor* Target)
+{
+	UseCounter++;
+}
+
+bool URangeRequirement::AreRequirementsMet(AActor* Owner, AActor* Target)
+{
+	float SqrDistance = FVector::DistSquared(Owner->GetTargetLocation(), Target->GetActorLocation());
+	return bRequireCloserThanThreshold ? SqrDistance < RangeThreshold * RangeThreshold : SqrDistance > RangeThreshold * RangeThreshold;
+}
+
+bool UCooldownRequirement::AreRequirementsMet(AActor* Owner, AActor* Target)
+{
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	return !TimerManager.IsTimerActive(TimerHandle);
+}
+
+void UCooldownRequirement::OnAttackExecuted(AActor* Owner, AActor* Target)
+{
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	TimerManager.SetTimer(TimerHandle, CooldownDuration, false);
+}
+
+bool USphereTraceRequirement::AreRequirementsMet(AActor* Owner, AActor* Target)
+{
+	TArray<AActor*> IgnoreActors;
+	IgnoreActors.Add(Owner);
+	IgnoreActors.Add(Target);
+	FVector Direction = (Target->GetActorLocation() - Owner->GetActorLocation()).GetSafeNormal();
+	FHitResult Hit;
+	UKismetSystemLibrary::SphereTraceSingle(GetWorld(),
+		Owner->GetActorLocation(),
+		Owner->GetActorLocation() + (Direction * TraceLength),
+		TraceRadius,
+		UEngineTypes::ConvertToTraceType(TraceChannel),
+		true,
+		IgnoreActors,
+		DebugDraw,
+		Hit,
+		true,
+		FLinearColor::Green,
+		FLinearColor::Red,
+		DebugDrawDuration);
+
+	return !Hit.bBlockingHit;
+}
--- Source/Enemies/Moves/MoveRequirements.h
+++ Source/Enemies/Moves/MoveRequirements.h
@@ -4,11 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include <Kismet/KismetSystemLibrary.h>
 #include "MoveRequirements.generated.h"
 
-/**
- * 
- */
 UCLASS(EditInlineNew)
 class UNREALPROJECT_API UBaseMoveRequirement : public UObject
 {
@@ -17,4 +15,92 @@ class UNREALPROJECT_API UBaseMoveRequirement : public UObject
 public:
 	UFUNCTION(BlueprintCallable)
 	virtual bool AreRequirementsMet(AActor* Owner, AActor* Target) { return true; };
+	virtual void OnAttackExecuted(AActor* Owner, AActor* Target) {};
+};
+
+UCLASS()
+class UNREALPROJECT_API UHealthMoveRequirement : public UBaseMoveRequirement
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool AreRequirementsMet(AActor* Owner, AActor* Target) override;
+
+private:
+	//If false check target hp
+	UPROPERTY(EditDefaultsOnly)
+	bool bCheckOwnerHealth = true;
+	UPROPERTY(EditDefaultsOnly)
+	bool bRequireHealthBelowThreshold = true;
+	UPROPERTY(EditDefaultsOnly)
+	bool bUsePercentageThreshold = true;
+	UPROPERTY(EditDefaultsOnly)
+	float Threshold = 0.5f;
+};
+
+UCLASS()
+class UNREALPROJECT_API ULimitedUsesRequirement : public UBaseMoveRequirement
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool AreRequirementsMet(AActor* Owner, AActor* Target) override;
+	virtual void OnAttackExecuted(AActor* Owner, AActor* Target) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	int NumberOfUses = 1;
+	int UseCounter = 0;
+};
+
+UCLASS()
+class UNREALPROJECT_API URangeRequirement : public UBaseMoveRequirement
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool AreRequirementsMet(AActor* Owner, AActor* Target) override;
+
+private:
+	//If true target needs to be closer than range if false target needs to be further
+	UPROPERTY(EditDefaultsOnly)
+	bool bRequireCloserThanThreshold = true;
+	UPROPERTY(EditDefaultsOnly)
+	float RangeThreshold = 900;
+};
+
+UCLASS()
+class UNREALPROJECT_API UCooldownRequirement : public UBaseMoveRequirement
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool AreRequirementsMet(AActor* Owner, AActor* Target);
+	virtual void OnAttackExecuted(AActor* Owner, AActor* Target);
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	float CooldownDuration = 10.f;
+	FTimerHandle TimerHandle;
+};
+
+UCLASS()
+class UNREALPROJECT_API USphereTraceRequirement : public UBaseMoveRequirement
+{
+	GENERATED_BODY()
+
+public:
+	virtual bool AreRequirementsMet(AActor* Owner, AActor* Target);
+	
+private:
+	UPROPERTY(EditAnywhere)
+	float TraceLength = 500.f;
+	UPROPERTY(EditAnywhere)
+	float TraceRadius = 75.f;
+	UPROPERTY(EditAnywhere)
+	TEnumAsByte<ECollisionChannel> TraceChannel = ECollisionChannel::ECC_GameTraceChannel4;
+	UPROPERTY(EditAnywhere)
+	TEnumAsByte<EDrawDebugTrace::Type> DebugDraw = EDrawDebugTrace::ForDuration;
+	UPROPERTY(EditAnywhere, meta = (EditCondition = "DebugDraw == EDrawDebugTrace::ForDuration"))
+	float DebugDrawDuration = 2.f;
 };
